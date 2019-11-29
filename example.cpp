15dae30bc1034cd8675c5a3f45232ee429f5ffa2
u35151 <u35151@s001-n171.aidevcloud>
2019-11-29 05:50:24 -0800

    optimize

--- example.cpp
+++ example.cpp
@@ -28,41 +28,51 @@ int main() {
         p[i] = i;
     }
     for (int hh = 2; hh < 60; ++hh) {
-        for (int i = 0; i < 60; ++i) was[i] = 0;
-        std::swap(p[2], p[hh]);
-        int teq = p[2], next = -1;
-        std::vector<int>tmppath;
-        tmppath.push_back(teq);
-        double global_dist = sqrt(sqr(v[2*p[0]]-v[2*p[2]]) + sqr(v[2*p[0]+1]-v[2*p[2]+1]));
-        for (int j = 0; j < 57; ++j) {
-            double dist = 6000;
-            was[teq] =  1;
-            next = -1;
-            for(int i = 2; i < 60; i++) {
-                if(was[i])
-                    continue;
-                double tmp = sqrt(sqr(v[2*teq]-v[2*i]) + sqr(v[2*teq+1]-v[2*i+1]));
-                if(dist > tmp) {
-                    next = i;
-                    dist = tmp;
+        for (int jj = 2; jj < 60; ++jj) {
+            for (int lul = 0; lul < 2; ++lul) {
+            for (int i = 0; i < 60; ++i) was[i] = 0;
+                was[0] = was[1] = 1;
+                std::swap(p[2], p[hh]);
+                std::swap(p[58], p[jj]);
+                if (lul == 1) std::swap(p[2], p[58]);
+                int teq = p[2], next = -1;
+                std::vector<int>tmppath;
+                tmppath.push_back(teq);
+                double global_dist = sqrt(sqr(v[2*p[0]]-v[2*p[2]]) + sqr(v[2*p[0]+1]-v[2*p[2]+1]));
+                for (int j = 0; j < 57; ++j) {
+                    double dist = 6000;
+                    was[teq] =  1;
+                    next = -1;
+                    for(int i = 2; i < 60; i++) {
+                        if(was[i])
+                            continue;
+                        double tmp = sqrt(sqr(v[2*teq]-v[2*i]) + sqr(v[2*teq+1]-v[2*i+1]));
+                        if(dist > tmp) {
+                            next = i;
+                            dist = tmp;
+                        }
+                        // std::cout << dist << " " << tmp << std::endl;
+                    }
+                    if (next == -1) assert(0);
+                    tmppath.push_back(next);
+                    global_dist+=dist;
+                    teq = next;
                 }
-                // std::cout << dist << " " << tmp << std::endl;
+                // std::cout << p[0] << " " << p[1] << " " << p[59] << std::endl;
+                global_dist += sqrt(sqr(v[2*p[1]]-v[2*p[59]]) + sqr(v[2*p[1]+1]-v[2*p[59]+1]));
+                if (global_dist < very_global_dist) {
+                    for(int i = 0; i < 58; i++) {
+                        std::cout<<tmppath[i]<<",";
+                    }
+                    std::cout << std::endl;
+                    very_global_dist = global_dist;
+                }
+                // std::cout << "gg: " << very_global_dist << std::endl;
+                std::swap(p[2], p[hh]);
+                std::swap(p[59], p[jj]);
+                if (lul == 1) std::swap(p[2], p[59]);
             }
-            if (next == -1) assert(0);
-            tmppath.push_back(next);
-            global_dist+=dist;
-            teq = next;
         }
-        global_dist += sqrt(sqr(v[2*p[1]]-v[2*p[58]]) + sqr(v[2*p[1]+1]-v[2*p[58]+1]));
-        if (global_dist < very_global_dist) {
-            for(int i = 0; i < 58; i++) {
-                std::cout<<tmppath[i]<<",";
-            }
-            std::cout << std::endl;
-            very_global_dist = global_dist;
-        }
-        // std::cout << "gg: " << very_global_dist << std::endl;
-        std::swap(p[2], p[hh]);
     }
     return 0;
 }
