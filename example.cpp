26d4baf51aa3a3942ba6493dddc3f4ea048cdae7
u35151 <u35151@s001-n171.aidevcloud>
2019-11-29 05:42:55 -0800

    wolf is gonna oof

--- example.cpp
+++ example.cpp
@@ -22,30 +22,18 @@ int main() {
     for(int i = 0; i < n; i++) {
         std::cin>>v[i*2]>>v[i*2+1];
     }
-    std::mt19937 rng(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-    std::uniform_int_distribution<int>dist[60];
-    for(int i = 1; i < 60; i++) {
-        dist[i] = std::uniform_int_distribution<int>(2, i);
-    }
-    
     double very_global_dist = 340257274234.;
-    while(true) {
-    // for(int forik = 0; forik < 10; forik++) {
-        // std::cout << "+" << std::endl;
+    std::vector<int>p(60);
+    for(int i = 0; i < 60; i++) {
+        p[i] = i;
+    }
+    for (int hh = 2; hh < 60; ++hh) {
         for (int i = 0; i < 60; ++i) was[i] = 0;
-        std::vector<int>p(60);
-        for(int i = 0; i < 60; i++) {
-            p[i] = i;
-        }
-        // for (int i = 60 - 1; i >= 2; --i) {
-        //     int j = dist[i](rng);
-        //     std::swap(p[i], p[j]);
-        // }
+        std::swap(p[2], p[hh]);
         int teq = p[2], next = -1;
-        // std::cout << p[0] << " " << p[1] << " " << p[2] << std::endl;
         std::vector<int>tmppath;
         tmppath.push_back(teq);
-        double global_dist = 0;
+        double global_dist = sqrt(sqr(v[2*p[0]]-v[2*p[2]]) + sqr(v[2*p[0]+1]-v[2*p[2]+1]));
         for (int j = 0; j < 57; ++j) {
             double dist = 6000;
             was[teq] =  1;
@@ -65,6 +53,7 @@ int main() {
             global_dist+=dist;
             teq = next;
         }
+        global_dist += sqrt(sqr(v[2*p[1]]-v[2*p[58]]) + sqr(v[2*p[1]+1]-v[2*p[58]+1]));
         if (global_dist < very_global_dist) {
             for(int i = 0; i < 58; i++) {
                 std::cout<<tmppath[i]<<",";
@@ -72,6 +61,8 @@ int main() {
             std::cout << std::endl;
             very_global_dist = global_dist;
         }
+        // std::cout << "gg: " << very_global_dist << std::endl;
+        std::swap(p[2], p[hh]);
     }
     return 0;
 }
