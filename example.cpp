eb7cdfa5c7e3b5e3f8f01a71c888c8ee7c3ebd5a
u35151 <u35151@s001-n171.aidevcloud>
2019-11-29 01:27:08 -0800

    random

--- example.cpp
+++ example.cpp
@@ -1,94 +1,100 @@
-#include <iostream>
-
+#include <CL/sycl.hpp>
+#include <fstream>
 #include <vector>
-#include <tuple>
-
-#include <cmath>
 #include <algorithm>
-#include <limits>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <float.h>
 
-struct Point {
-    float x;
-    float y;
-    
-    Point(float x, float y)
-        : x(x), y(y)
-    { }
-    
-    static float distance(const Point& p1, const Point& p2) {
-        float result = 0.f;
-        result += (p1.x - p2.x)*(p1.x - p2.x);
-        result += (p1.y - p2.y)*(p1.y - p2.y);
-        
-        return std::sqrt(result);
-    }
-};
+using namespace cl::sycl;
 
-std::vector<float> find_distances(const std::vector<Point>& points);
-void find_shortest_path(const std::vector<float>& distances);
+cpu_selector cpu_sel;
+// gpu_selector gpu_sel;
+queue cpu_queue = queue(cpu_sel);
+// queue gpu_queue = queue(gpu_sel);
+
+float sqr(float x) {
+    return x * x;
+}
 
 int main() {
-    std::vector<Point> points;
-    {
-        int number_of_points = 0;
-        std::cin >> number_of_points;
-    
-        for (size_t i = 0; i < number_of_points; i++) {
-            float x, y;
-            std::cin >> x >> y;
-            points.emplace_back(x, y);
-        }
+    std::ifstream data("data.txt");
+    int n;
+    data >> n;
+    std::vector <float> v(2 * n);
+    for (int i = 0; i < 2 * n; ++i) {
+        data >> v[i];
     }
-    
-    std::vector<float> distances = find_distances(points);
-    find_shortest_path(distances);
-    
+    // std::cout << std::endl;
+    buffer <float, 1> a(v.data(), range<1>(2 * n));
+    float m_res = 1000000;
+    std::vector <int> result(n + 1);
+    buffer <int, 1> res(result.data(), range<1>(n));
+    buffer <float, 1> min_res(&m_res, range<1>(1));
+    // for (int forik = 0; forik < 10; ++forik) {
+    while(true) {
+        cpu_queue.submit([&](handler &cgh) {
+            // stream out(1000000, 1000000, cgh);
+            stream out(1024, 80, cgh);
+            // stream out2(100000, 100000, cgh);
+            srand(time(0));
+            auto A = a.get_access<access::mode::read_write>(cgh);
+            auto RES = res.get_access<access::mode::read_write>(cgh);
+            auto MIN_RES = min_res.get_access<access::mode::read_write>(cgh);
+            // cgh.parallel_for<class For2>(range<1>(120), [=](id<1> index) {
+                // out << index << A[index] << endl;//  << " ";
+                // out << index;
+            // });
+            // out << "lul";
+            // static std::mt19937 rng(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+            cgh.parallel_for<class For1>(range<1>(24), [=](id<1> index) {
+                // out << index;
+                // buffer <int, 1> p(range<1>(n));
+                // auto P = p.get_access<access::mode::read_write>(cgh);
+                // int *p = new int[n];
+                // for (int i = 0; i < n; ++i) {
+                    // p[i] = i;
+                    // out << p[i] << " ";
+                // }
+                // out << endl;
+                int p[60];
+                for (int i = 0; i < 60; ++i) {
+                    p[i] = i;
+                }
+                for (int i = 60 - 1; i >= 2; --i) {
+                    // std::uniform_int_distribution<int> dist(1, i);
+                    // int j = dist(rng);
+                    int j = rand() % (i - 1) + 2;
+                    std::swap(p[i], p[j]);
+                }
+                std::swap(p[1], p[60 - 1]);
+                float dist = 0.;
+                for (int i = 1; i < 60; ++i) {
+                    dist += sqr(A[2 * p[i]] - A[2 * p[i-1]]) + sqr(A[2 * p[i] + 1] - A[2 * p[i-1] + 1]);
+                }
+                if (dist < MIN_RES[0]) {
+                    for (int i = 0; i < n; ++i) {
+                        RES[i] = p[i];
+                    }
+                    // RES[i] = dist;
+                    MIN_RES[0] = dist;
+                }
+                // out << "p: ";
+                // for (int i = 0; i < 60; ++i) {
+                    // out << p[i] << " ";
+                // }
+                // out << 5 << endl;
+                // out << endl; 
+            });
+
+        });
+        cpu_queue.wait();
+        // std::cout << m_res << ": ";
+        for (int i = 0; i < n; ++i) {
+            std::cout << result[i] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
-}
-
-std::vector<float> find_distances(const std::vector<Point>& points) {
-    const size_t n = points.size();
-    
-    std::vector<float> distances(n*n, 0.f);
-    for (size_t row = 0; row < n; row++) {
-        for (size_t col = 0; col < n; col++) {
-            distances[row*n + col] = Point::distance(points[row], points[col]);
-        }
-    }
-    
-    return distances;
-}
-
-void find_shortest_path(const std::vector<float>& distances) {
-    const size_t n = std::sqrt(distances.size());
-    const size_t start_point = 0;
-    const size_t end_point = 1;
-    
-    std::vector<size_t> point_indices;
-    for (size_t i = 2; i < n; i++) {
-            point_indices.push_back(i);
-    }
-    
-    auto min_path = std::numeric_limits<float>::max();
-    do {
-        std::vector<size_t> cur_path;
-        
-        float cur_path_weight = 0.f;
-        size_t cur_point = start_point;
-        
-        for (size_t i = 0; i < point_indices.size(); i++) {
-            cur_path_weight += distances[cur_point*n + point_indices[i]];
-            cur_point = point_indices[i];
-            cur_path.push_back(cur_point);
-        }
-        cur_path_weight += distances[cur_point*n + end_point];
-        
-        if (min_path > cur_path_weight) {
-            min_path = cur_path_weight;
-            for (size_t idx : cur_path) {
-                std::cout << idx << ",";
-            }
-            std::cout << std::endl;
-        }
-    } while (std::next_permutation(point_indices.begin(), point_indices.end()));
 }
