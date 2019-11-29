65913f6a2a612f56dfbe7eb9119db9ea5434224a
u35151 <u35151@s001-n171.aidevcloud>
2019-11-29 05:18:53 -0800

    wolf

--- example.cpp
+++ example.cpp
@@ -3,104 +3,75 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <cstdlib>
 #include <ctime>
 #include <float.h>
 
 using namespace cl::sycl;
 
-cpu_selector cpu_sel;
-gpu_selector gpu_sel;
-queue cpu_queue = queue(cpu_sel);
-queue gpu_queue = queue(gpu_sel);
-
-float sqr(float x) {
-    return x * x;
+double sqr(double a) {
+    return a*a;
 }
 
-#define RRRRRRRRAND_MAX 32767  
+int was[60];
 
 int main() {
-    static unsigned long int next = 1;
-    auto rand = [&](void) {
-        next = next * 1103515245 + 12345;
-        return (unsigned int)(next/65536) % (RRRRRRRRAND_MAX + 1);
-    };
-    auto srand = [&](unsigned int seed) {
-        next = seed;
-    };
     int n;
-    std::cin >> n;
-    std::vector <float> v(2 * n);
-    for (int i = 0; i < 2 * n; ++i) {
-        std::cin >> v[i];
+    std::cin>>n;
+    std::vector<float>v(2 * n);
+    for(int i = 0; i < n; i++) {
+        std::cin>>v[i*2]>>v[i*2+1];
     }
-    // std::cout << std::endl;
-    buffer <float, 1> a(v.data(), range<1>(2 * n));
-    float m_res = 1000000;
-    std::vector <int> result(n);
-    buffer <int, 1> res(result.data(), range<1>(n));
-    buffer <float, 1> min_res(&m_res, range<1>(1));
-    // for (int forik = 0; forik < 10; ++forik) {
+    std::mt19937 rng(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+    std::uniform_int_distribution<int>dist[60];
+    for(int i = 1; i < 60; i++) {
+        dist[i] = std::uniform_int_distribution<int>(2, i);
+    }
+    
+    double very_global_dist = 340257274234.;
     while(true) {
-        gpu_queue.submit([&](handler &cgh) {
-            auto A = a.get_access<access::mode::read>(cgh);
-            auto RES = res.get_access<access::mode::write>(cgh);
-            auto MIN_RES = min_res.get_access<access::mode::read_write>(cgh);
-            cgh.parallel_for<class For1>(range<1>(24), [=](id<1> index) {
-                int p[60];
-                for (int i = 0; i < 60; ++i) {
-                    p[i] = i;
-                }
-                for (int i = 60 - 1; i >= 2; --i) {
-                    int j = rand() % (i - 1) + 2;
-                    std::swap(p[i], p[j]);
-                }
-                std::swap(p[1], p[60 - 1]);
-                float dist = 0.;
-                for (int i = 1; i < 60; ++i) {
-                    dist += sqrt(sqr(A[2 * p[i]] - A[2 * p[i-1]]) + sqr(A[2 * p[i] + 1] - A[2 * p[i-1] + 1]));
-                }
-                if (dist < MIN_RES[0]) {
-                    for (int i = 0; i < n; ++i) {
-                        RES[i] = p[i];
-                    }
-                    MIN_RES[0] = dist;
-                }
-            });
-        });
-        cpu_queue.submit([&](handler &cgh) {
-            auto A = a.get_access<access::mode::read>(cgh);
-            auto RES = res.get_access<access::mode::write>(cgh);
-            auto MIN_RES = min_res.get_access<access::mode::read_write>(cgh);
-            cgh.parallel_for<class For2>(range<1>(24), [=](id<1> index) {
-                int p[60];
-                for (int i = 0; i < 60; ++i) {
-                    p[i] = i;
-                }
-                for (int i = 60 - 1; i >= 2; --i) {
-                    int j = rand() % (i - 1) + 2;
-                    std::swap(p[i], p[j]);
-                }
-                std::swap(p[1], p[60 - 1]);
-                float dist = 0.;
-                for (int i = 1; i < 60; ++i) {
-                    dist += sqrt(sqr(A[2 * p[i]] - A[2 * p[i-1]]) + sqr(A[2 * p[i] + 1] - A[2 * p[i-1] + 1]));
-                }
-                if (dist < MIN_RES[0]) {
-                    for (int i = 0; i < n; ++i) {
-                        RES[i] = p[i];
-                    }
-                    MIN_RES[0] = dist;
-                }
-            });
-        });
-        cpu_queue.wait();
-        gpu_queue.wait();
-        auto acc = res.get_access<access::mode::read>();
-        for (int i = 1; i < n - 1; ++i) {
-            std::cout << acc[i] << ",";
+    // for(int forik = 0; forik < 10; forik++) {
+        // std::cout << "+" << std::endl;
+        for (int i = 0; i < 60; ++i) was[i] = 0;
+        std::vector<int>p(60);
+        for(int i = 0; i < 60; i++) {
+            p[i] = i;
         }
-        std::cout << std::endl;
+        // for (int i = 60 - 1; i >= 2; --i) {
+        //     int j = dist[i](rng);
+        //     std::swap(p[i], p[j]);
+        // }
+        int teq = p[2], next = -1;
+        // std::cout << p[0] << " " << p[1] << " " << p[2] << std::endl;
+        std::vector<int>tmppath;
+        tmppath.push_back(teq);
+        double global_dist = 0;
+        for (int j = 0; j < 57; ++j) {
+            double dist = 6000;
+            was[teq] =  1;
+            next = -1;
+            for(int i = 2; i < 60; i++) {
+                if(was[i])
+                    continue;
+                double tmp = sqrt(sqr(v[2*teq]-v[2*i]) + sqr(v[2*teq+1]-v[2*i+1]));
+                if(dist > tmp) {
+                    next = i;
+                    dist = tmp;
+                }
+                // std::cout << dist << " " << tmp << std::endl;
+            }
+            if (next == -1) assert(0);
+            tmppath.push_back(next);
+            global_dist+=dist;
+            teq = next;
+        }
+        if (global_dist < very_global_dist) {
+            for(int i = 0; i < 58; i++) {
+                std::cout<<tmppath[i]<<",";
+            }
+            std::cout << std::endl;
+            very_global_dist = global_dist;
+        }
     }
     return 0;
 }
